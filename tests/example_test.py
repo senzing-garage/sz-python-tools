1d8939293ffa9528712d2736b343219e13fcd0b4
docktermj <michael@dockter.com>
2025-10-29 13:05:43 -0400

    #259 Fix linting issues - 8

---
+++ tests/example_test.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+# Example testcases
+# -----------------------------------------------------------------------------
+
+
+def test_example() -> None:
+    """Example test"""
+    a_string = "This is a test string"
+    assert a_string == "This is a test string"
